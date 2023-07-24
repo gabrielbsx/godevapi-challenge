d0686fbe96e76c091ef8f434c9c6d73d6efa943a
Gabriel Barbosa <gabrielturing@gmail.com>
2023-07-24 20:24:37 -0300

    feat: contract with repository and entity connector

--- apps/connector-manager-api/src/domain/repository/connector.ts
+++ apps/connector-manager-api/src/domain/repository/connector.ts
@@ -1,4 +1,9 @@
 import { ConnectorEntity } from '../entity/connector';
 import { Repository } from './repository';
 
-export abstract class ConnectorRepository extends Repository<ConnectorEntity> {}
+export abstract class ConnectorRepository extends Repository<ConnectorEntity> {
+  public abstract findByName(name: string): Promise<ConnectorEntity[]>;
+  public abstract findByCategory(category: string): Promise<ConnectorEntity[]>;
+  public abstract findByType(type: string): Promise<ConnectorEntity[]>;
+  public abstract findByPrivacy(privacy: string): Promise<ConnectorEntity[]>;
+}
--- apps/connector-manager-api/src/domain/repository/repository.ts
+++ apps/connector-manager-api/src/domain/repository/repository.ts
@@ -1,3 +1,7 @@
 export abstract class Repository<T> {
   abstract create(data: T): Promise<void>;
+  abstract all(): Promise<T[]>;
+  abstract findById(id: string): Promise<T>;
+  abstract update(id: string, data: T): Promise<void>;
+  abstract delete(id: string): Promise<void>;
 }
