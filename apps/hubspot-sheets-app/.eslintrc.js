ecb4e41810f568f8082938de2134ed1689e2ad32
Gabriel Barbosa <gabrielturing@gmail.com>
2023-07-25 03:08:28 -0300

    chore: add eslint and tsconfig setup

---
+++ apps/hubspot-sheets-app/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    env: {
+        "es2021": true,
+        "node": true
+    },
+    extends: 'standard-with-typescript',
+    parserOptions: {
+        project: 'tsconfig.json',
+        tsconfigRootDir: __dirname,
+        sourceType: 'module',
+    },
+    ignorePatterns: ['.eslintrc.js'],
+    rules: {
+    }
+}
