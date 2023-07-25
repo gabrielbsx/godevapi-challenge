8c3de53659d9a49d99da308f145ff95d186c51d2
Gabriel Barbosa <gabrielturing@gmail.com>
2023-07-25 01:16:07 -0300

    refactor: change findall repository behavior

--- apps/connector-manager-api/src/domain/repository/repository.ts
+++ apps/connector-manager-api/src/domain/repository/repository.ts
@@ -1,6 +1,6 @@
 export abstract class Repository<T> {
   abstract create(data: T): Promise<void>;
-  abstract all(): Promise<T[]>;
+  abstract findAll(): Promise<T[]>;
   abstract findById(id: string): Promise<T>;
   abstract update(id: string, data: T): Promise<void>;
   abstract delete(id: string): Promise<void>;
