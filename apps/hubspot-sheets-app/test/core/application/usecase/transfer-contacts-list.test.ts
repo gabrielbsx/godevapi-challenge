a1123f0c2db52a8cb5bcdfadd296ce8d6c006fd0
Gabriel Barbosa <gabrielturing@gmail.com>
2023-07-25 04:46:42 -0300

    test: ensure transfer contacts list is impl

---
+++ apps/hubspot-sheets-app/test/core/application/usecase/transfer-contacts-list.test.ts
@@ -0,0 +1,40 @@
+import { TransferContactsListUseCaseImpl } from '@core/application/usecase/transfer-contacts-list.usecase'
+import { type Contact } from '@core/domain/entity/contact'
+import { type ContactStorageService } from '@core/domain/port/contact-storage-service'
+import { InMemoryContactService } from '@infra/adapter/service/in-memory-contact.service'
+import { mockContact } from 'test/core/domain/mocks/mock-contacts'
+import { describe, expect, test } from 'vitest'
+
+interface SutTypes {
+  transferContactsListUseCase: TransferContactsListUseCaseImpl
+  toContactStorageService: ContactStorageService
+  fromContactStorageService: ContactStorageService
+}
+
+const makeSut = (): SutTypes => {
+  const toContactStorageService: ContactStorageService = new InMemoryContactService()
+  const fromContactStorageService: ContactStorageService = new InMemoryContactService()
+  const transferContactsListUseCase = new TransferContactsListUseCaseImpl(toContactStorageService, fromContactStorageService)
+  return { transferContactsListUseCase, toContactStorageService, fromContactStorageService }
+}
+
+describe('TransferContactsListUseCaseImpl', () => {
+  test('should return true', async () => {
+    const { transferContactsListUseCase } = makeSut()
+    const result = await transferContactsListUseCase.execute()
+    expect(result).toEqual(true)
+  })
+
+  test('should be transferred', async () => {
+    const { transferContactsListUseCase, toContactStorageService, fromContactStorageService } = makeSut()
+    const mockContacts: Contact[] = []
+    expect([]).toEqual(await toContactStorageService.getContacts())
+    expect([]).toEqual(await fromContactStorageService.getContacts())
+    Array.from({ length: 10 }).forEach(() => mockContacts.push(mockContact()))
+    await fromContactStorageService.saveContacts(mockContacts)
+    const result = await transferContactsListUseCase.execute()
+    expect(result).toEqual(true)
+    const toContacts = await toContactStorageService.getContacts()
+    expect(mockContacts).toEqual(toContacts)
+  })
+})
