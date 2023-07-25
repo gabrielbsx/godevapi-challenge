3cbcf9c98a7823a1b9755e2fb20c69e53d89285b
Gabriel Barbosa <gabrielturing@gmail.com>
2023-07-25 19:27:54 -0300

    refactor: main app

--- apps/hubspot-sheets-app/src/main.ts
+++ apps/hubspot-sheets-app/src/main.ts
@@ -1,21 +1,6 @@
-import { TransferContactsListUseCaseImpl } from '@core/application/usecase/transfer-contacts-list.usecase'
-import { GoogleSheetsServiceImpl } from '@infra/adapter/service/google-sheets/google-sheets.service'
-import { HubspotContactService } from '@infra/adapter/service/hubspot/hubspot-contact.service'
-import { config } from 'dotenv'
+import { Server } from './main/server'
 
-config()
-
-const main = async (): Promise<void> => {
-  const sheetId = '1FVlfOM3pAxCuqzuPep8_vFidQ8FSH2jODecAD7ieNXc'
-  const googleSheetsService = new GoogleSheetsServiceImpl()
-  const hubspotContactService = new HubspotContactService()
-  const transferContactsListUseCase = new TransferContactsListUseCaseImpl(
-    hubspotContactService,
-    googleSheetsService
-  )
-  void transferContactsListUseCase.execute({ sheetId })
-  await googleSheetsService.getContacts(sheetId)
-  console.log('Done')
-}
-
-main().catch((error) => { console.error(error) })
+const server = new Server()
+server.bootstrap()
+server.start()
+//   const sheetId = '1FVlfOM3pAxCuqzuPep8_vFidQ8FSH2jODecAD7ieNXc'
