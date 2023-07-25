dc3855fb9c2ac90932e62c88d8637add68917115
Gabriel Barbosa <gabrielturing@gmail.com>
2023-07-25 04:47:06 -0300

    feat: nevermind i forget to commit in memory service

---
+++ apps/hubspot-sheets-app/src/infra/adapter/service/in-memory-contact.service.ts
@@ -0,0 +1,14 @@
+import { type Contact } from '@core/domain/entity/contact'
+import { type ContactStorageService } from '@core/domain/port/contact-storage-service'
+
+export class InMemoryContactService implements ContactStorageService {
+  private readonly contacts: Contact[] = []
+
+  public async getContacts (): Promise<Contact[]> {
+    return this.contacts
+  }
+
+  public async saveContacts (contacts: Contact[]): Promise<void> {
+    this.contacts.push(...contacts)
+  }
+}
